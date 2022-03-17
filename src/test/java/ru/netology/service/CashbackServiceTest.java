e6a4732a006d7181a19ba683f0f1043cb6ec0a8f
GafarovEvgenii <91888626+GafarovEvgenii@users.noreply.github.com>
2022-03-17 14:32:37 +0300

    Update CashbackServiceTest.java

--- src/test/java/ru/netology/service/CashbackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackServiceTest.java
@@ -1,4 +1,27 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 public class CashbackServiceTest {
+  CashbackServiceTest = new CashbackServiceTest();
+  
+   @Test
+    public void shouldAmountLessThan1000() {
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmountMoreThan1000() {
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmount1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
   
 }
